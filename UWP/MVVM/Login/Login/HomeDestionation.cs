7c3061efcb66b83685a1a5d69e5780e7cc5b2149
amesic <37384187+amesic@users.noreply.github.com>
2018-05-03 00:35:13 +0200

    MVVM, pokusaj prvi, treba se popraviti

---
+++ UWP/MVVM/Login/Login/HomeDestionation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class HomeDestionation
+    {
+        public static List<Smjestaj> Smjestaji;
+        public static List<Osoba> Korisnici;
+        public HomeDestionation()
+        {
+            Smjestaji = new List<Smjestaj>();
+            Korisnici = new List<Osoba>();
+        }
+    }
+}
